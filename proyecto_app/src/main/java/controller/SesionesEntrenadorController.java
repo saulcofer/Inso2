ba6639cfcabc5738a67c892bef613d3bdfbbc22c
jvegag01 <jvegag01@estudiantes.unileon.es>
2024-05-22 18:51:25 +0200

    arreglado y hecho todo

--- proyecto_app/src/main/java/controller/SesionesEntrenadorController.java
+++ proyecto_app/src/main/java/controller/SesionesEntrenadorController.java
@@ -31,9 +31,7 @@ public class SesionesEntrenadorController implements Serializable{
     private Usuario user;
     private String accion;
     private List<Instalacion>listainstalaciones;
-    private List<Instalacion>listamaterial;
-    private Instalacion instalacion;
-    private Instalacion material;
+    private String[] selected_instalaciones;
     
     @EJB
     private SesionFacadeLocal sesionEJB;
@@ -45,52 +43,38 @@ public class SesionesEntrenadorController implements Serializable{
     public void init(){
         user=(Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
         listasesiones = user.getSesiones();
-        listainstalaciones=instalacionEJB.findInstalaciones();
-        listamaterial=instalacionEJB.findMateriales();       
+        listainstalaciones=instalacionEJB.findAll();   
     }
     
     public void establecerSesionAñadir(){
         this.setAccion("R");
         this.sesion = new Sesion();
-        this.instalacion=null;
-        this.material=null;
+        String nuevas_instalaciones[]=new String[0];
+        this.selected_instalaciones=nuevas_instalaciones;
     }
     
     public void insertarSesion(){
-        System.out.println("entra");
         List<Usuario> usuarios = new ArrayList<>();
-        List<Instalacion> instalaciones = new ArrayList<>();
+        List<Instalacion> instalaciones = obtenerInstalaciones(this.selected_instalaciones);
         sesion.setInstalaciones(instalaciones);
         sesion.setUsuarios(usuarios);
         sesion.getUsuarios().add(user);
         user.getSesiones().add(sesion);
-        System.out.println(sesion);
-        System.out.println(instalacion);
-        System.out.println(material);
-        if(instalacion!=null){
-            sesion.getInstalaciones().add(instalacion);
-            instalacion.getSesiones().add(sesion);
-        }
-        if(material!=null){
-            sesion.getInstalaciones().add(material);
-            material.getSesiones().add(sesion);
-        }
         sesionEJB.create(this.sesion);
         listasesiones = user.getSesiones();
     }
     
     public void establecerSesionEliminar(Sesion sesion){
-        System.out.println(instalacion);
-        System.out.println(material);
         this.setAccion("E");
         this.sesion = sesion;
-        this.instalacion=getInstalacion2(sesion);
-        this.material=getMaterial2(sesion);
     }
     
     public void eliminarSesion(){
         for(Usuario usuario : sesion.getUsuarios()) {
             usuario.getSesiones().remove(sesion);
+        }
+        for(Instalacion instalacion : sesion.getInstalaciones()) {
+            instalacion.getSesiones().remove(sesion);
         }
 
         sesionEJB.remove(this.sesion);
@@ -100,16 +84,27 @@ public class SesionesEntrenadorController implements Serializable{
     public void establecerSesionModificar(Sesion sesion){
         this.setAccion("M");
         this.sesion = sesion;
-        this.instalacion=getInstalacion2(sesion);
-        this.material=getMaterial2(sesion);
+        this.selected_instalaciones=obtenerInstalaciones_names(this.sesion.getInstalaciones());
     }
     
     public void modificarSesion(){
-        List<Instalacion> instalaciones = new ArrayList<>();
-        sesion.setInstalaciones(instalaciones);
-        sesion.getInstalaciones().add(this.instalacion);
-        System.out.println(sesion.getInstalaciones());
+        List<Instalacion> instalacionesActuales = sesion.getInstalaciones();
+        List<Instalacion> nuevasInstalaciones = obtenerInstalaciones(this.selected_instalaciones);
+        // Eliminar instalaciones que ya no están seleccionadas
+        instalacionesActuales.removeIf(ins -> !nuevasInstalaciones.contains(ins));
+
+        // Añadir nuevas instalaciones seleccionadas
+        for (Instalacion nuevaInstalacion : nuevasInstalaciones) {
+            if (!instalacionesActuales.contains(nuevaInstalacion)) {
+                instalacionesActuales.add(nuevaInstalacion);
+                System.out.println("Instalacion Añadida");
+            }
+        }
+
+        sesion.setInstalaciones(instalacionesActuales);
+
         sesionEJB.edit(this.sesion);
+        
         listasesiones = user.getSesiones();
     }
     
@@ -124,53 +119,60 @@ public class SesionesEntrenadorController implements Serializable{
     }
     
     public String getInstalacion(Sesion ses){
-        String instalacion2="No tiene instalación asignada";
-        if(getInstalacionLista(ses.getInstalaciones())!=null){
-            instalacion2=getInstalacionLista(ses.getInstalaciones()).getNombre()+" ("+getInstalacionLista(ses.getInstalaciones()).getDescripcion()+")";
+        String instalacion2="No tiene instalaciones asignadas";
+        if(!getInstalacionLista(ses.getInstalaciones()).equals("")){
+            instalacion2=getInstalacionLista(ses.getInstalaciones());
         }    
         
         return instalacion2;
     }
     
-    public Instalacion getInstalacionLista(List<Instalacion> lista){
-        for (Instalacion instalacion : lista) {
-            if ("I".equals(instalacion.getTipo())) {
-                return instalacion;
-            }
-    }    
-        Instalacion instalacion=null;
-        return instalacion;
-    }
-    
-    public String getMaterial(Sesion ses){
-        String material2="No tiene material asignado";
-        if(getMaterialLista(ses.getInstalaciones())!=null){
-            material2=getMaterialLista(ses.getInstalaciones()).getNombre()+" ("+getMaterialLista(ses.getInstalaciones()).getDescripcion()+")";
-        }    
-        
-        return material2;
-    }
-    
-    public Instalacion getMaterialLista(List<Instalacion> lista){
-        for (Instalacion instalacion : lista) {
-            if ("M".equals(instalacion.getTipo())) {
-                return instalacion;
-            }
-    }    
-        Instalacion instalacion=null;
-        return instalacion;
-    }
-    
-    public Instalacion getMaterial2(Sesion ses){
-        for (Instalacion instalacion : ses.getInstalaciones()) {
-            if ("M".equals(instalacion.getTipo())) {
-                return instalacion;
-            }
-    }    
-        Instalacion instalacion=null;
-        return instalacion;
-    }
+    public String getInstalacionLista(List<Instalacion> lista){
+        String listainstalaciones2="";
 
+        // Recorre la lista y añade los nombres de las instalaciones, separados por comas.
+        for (Instalacion instalacion : lista) {
+            listainstalaciones2=listainstalaciones2+" ";
+            listainstalaciones2=listainstalaciones2+instalacion.getNombre();
+            listainstalaciones2=listainstalaciones2+" ("+instalacion.getDescripcion()+")";
+        }
+
+    // Convierte el StringBuilder a String y lo devuelve.
+        return listainstalaciones2;
+
+    }
+    
+   
+
+    public List<Instalacion> obtenerInstalaciones(String[] names){
+        List<Instalacion> result = new ArrayList<>();
+        int i;
+        boolean found;
+        
+        for(Instalacion ins : this.listainstalaciones){
+            i=0;
+            found=false;
+            while(!found && i<names.length){
+                if(names[i].equals(ins.getNombre())){
+                    result.add(ins);
+                    found=true;
+                }
+                i++;
+            }
+        }
+        return result;
+    }
+    
+    public String[] obtenerInstalaciones_names (List<Instalacion>instalaciones){
+        String[] result = new String[instalaciones.size()];
+
+        for(int i=0;i<instalaciones.size();i++){
+            result[i] = instalaciones.get(i).getNombre();
+        }
+        return result;
+     }
+
+    
     public List<Sesion> getListasesiones() {
         return listasesiones;
     }
@@ -219,36 +221,20 @@ public class SesionesEntrenadorController implements Serializable{
         this.listainstalaciones = listainstalaciones;
     }
 
-    public List<Instalacion> getListamaterial() {
-        return listamaterial;
-    }
-
-    public void setListamaterial(List<Instalacion> listamaterial) {
-        this.listamaterial = listamaterial;
-    }
-
-    public Instalacion getInstalacion() {
-        return instalacion;
-    }
-
-    public void setInstalacion(Instalacion instalacion) {
-        this.instalacion = instalacion;
-    }
-
-    public Instalacion getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(Instalacion material) {
-        this.material = material;
-    }
-
     public InstalacionFacadeLocal getInstalacionEJB() {
         return instalacionEJB;
     }
 
     public void setInstalacionEJB(InstalacionFacadeLocal instalacionEJB) {
         this.instalacionEJB = instalacionEJB;
+    }
+
+    public String[] getSelected_instalaciones() {
+        return selected_instalaciones;
+    }
+
+    public void setSelected_instalaciones(String[] selected_instalaciones) {
+        this.selected_instalaciones = selected_instalaciones;
     }
 
     
